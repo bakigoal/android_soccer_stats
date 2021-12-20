3dcc0ab888f40a8204600f3f0d4e64980a4ca7d3
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-20 13:14:57 +0500

    use glide and caverock to decode svg

---
+++ app/src/main/java/com/bakigoal/soccerstats/config/svg/SvgGlideModule.kt
@@ -0,0 +1,37 @@
+package com.bakigoal.soccerstats.config.svg
+
+import android.content.Context
+import android.graphics.drawable.PictureDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.Options
+import com.bumptech.glide.load.engine.Resource
+import com.bumptech.glide.load.resource.SimpleResource
+import com.bumptech.glide.load.resource.transcode.ResourceTranscoder
+import com.bumptech.glide.module.AppGlideModule
+import com.caverock.androidsvg.SVG
+import java.io.InputStream
+
+@GlideModule
+class SvgGlideModule : AppGlideModule() {
+
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        registry
+            .register(SVG::class.java, PictureDrawable::class.java, SvgDrawableTranscoder())
+            .append(InputStream::class.java, SVG::class.java, SvgDecoder())
+    }
+
+    // Disable manifest parsing to avoid adding similar modules twice.
+    override fun isManifestParsingEnabled(): Boolean {
+        return false
+    }
+
+    class SvgDrawableTranscoder : ResourceTranscoder<SVG, PictureDrawable> {
+        override fun transcode(resource: Resource<SVG>, opts: Options): Resource<PictureDrawable> {
+            val svg = resource.get()
+            val picture = svg.renderToPicture()
+            return SimpleResource(PictureDrawable(picture))
+        }
+    }
+}
