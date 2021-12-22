bc334eb58be27e4a25e6a40f959c85364d072cc5
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-22 20:52:24 +0500

    add TableView

--- app/src/main/java/com/bakigoal/soccerstats/ui/fragments/TableFragment.kt
+++ app/src/main/java/com/bakigoal/soccerstats/ui/fragments/TableFragment.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bakigoal.soccerstats.R
 import com.bakigoal.soccerstats.databinding.FragmentTableBinding
+import com.bakigoal.soccerstats.domain.Standings
 import com.bakigoal.soccerstats.ui.viewModels.TableViewModel
 
 private const val ARG_PARAM1 = "leagueId"
@@ -45,7 +51,24 @@ class TableFragment : Fragment() {
 
         binding.viewModel = viewModel
 
+        viewModel.standings.observe(viewLifecycleOwner, {
+            it?.apply { fillTable(binding.standingsTableLayout, it) }
+        })
+
         return binding.root
+    }
+
+    private fun fillTable(tableLayout: TableLayout, standings: Standings) {
+        standings.standings.forEach {
+            val tableRow = TableRow(requireActivity())
+            tableRow.layoutParams = ViewGroup.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+
+            val textView = TextView(requireActivity())
+            textView.text = it.toString()
+
+            tableRow.addView(textView)
+            tableLayout.addView(tableRow)
+        }
     }
 
     companion object {
